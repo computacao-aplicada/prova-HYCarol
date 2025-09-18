61d9a8ea257dbd409e625d455f116bd147d541f4
HYCarol <120440173+HYCarol@users.noreply.github.com>
2025-09-17 22:02:49 -0300

    prova concluida

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -5,12 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidadorTest {
 
+    //Testa para ver se o cpf eh correto com ou sem a mascara
     @Test
     void deveValidarCPFValido() {
         assertTrue(Validador.validarCPF("529.982.247-25"));
         assertTrue(Validador.validarCPF("52998224725"));
     }
 
+    //Rejeita o teste com entrada errada
     @Test
     void deveRejeitarEntradasInvalidas() {
         assertFalse(Validador.validarCPF(null));
@@ -19,12 +21,14 @@ public class ValidadorTest {
         assertFalse(Validador.validarCPF("00000000000"));
     }
 
+    //Rejeita com o cpf com tamanho que nao seja de 10 ou de 12 digitos
     @Test
     void deveRejeitarTamanhosIncorretos() {
         assertFalse(Validador.validarCPF("935.411.347-8"));   // 10 dígitos
         assertFalse(Validador.validarCPF("935.411.347-800")); // 12 dígitos
     }
 
+    //Faz o calculo do DV e rejeita o incorreto
     @Test
     void deveRejeitarDVIncorreto() {
         assertFalse(Validador.validarCPF("529.982.247-24"));
