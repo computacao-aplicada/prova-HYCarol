ea2e073c10a583df89f57fdf6d79236ad3eee431
HYCarol <120440173+HYCarol@users.noreply.github.com>
2025-09-17 21:20:40 -0300

    commit 1

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -4,4 +4,10 @@ import org.junit.jupiter.api.Test;
 
 
 public class ValidadorTest {
+
+    @Test
+    void deveValidarCPFValido() {
+        assertTrue(Validador.validarCPF("529.982.247-25"));
+        assertTrue(Validador.validarCPF("52998224725"));
+    }
 }
